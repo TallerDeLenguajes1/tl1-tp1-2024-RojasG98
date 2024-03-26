f72718dc58c448446cac7ff17a414d3a1203118c
GabielR98 <rojista.098g@gmail.com>
2024-03-26 13:45:49 -0300

    Realizo el programa solicitado en el apartado 3_e del practico

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main(){
-    cout << "hola mundo" << endl;
+    printf("hola mundo");
+    int numero = 59;
+    int *punt;
+    punt = &numero;
+    printf("\nel contenido del puntero es %d", *punt);
+    printf("\nLa dirección de memoria almacenada por el puntero: %p", punt);
+    printf("\nla dirección de memoria de la variable: %p", &numero);
+    printf("\nla dirección de memoria del puntero %p", &punt);
+    printf("\nel tamaño de memoria utilizado por la variable %d bytes", sizeof(*punt));
+
     return 0;
 }
