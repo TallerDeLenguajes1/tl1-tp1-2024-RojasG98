c96c944bcfed2f2bee7a63b326560635b100f8cc
GabielR98 <rojista.098g@gmail.com>
2024-03-28 20:35:23 -0300

    ejercicio 2.4 completado

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -5,9 +5,14 @@ float elevarAlCuadrado(float numero);
 
 void elevarAlCuadrado2(float *numero);
 
+void Invertir(int *a,int *b);
+
+void orden(int *a,int *b);
+
 
 int main(){
    float num1,num2;
+   int variable, a, b;
    printf("ingrese un numero: \n");
    scanf("%f",&num1);
    printf("su cuadrado es %.2f\n", elevarAlCuadrado(num1));
@@ -16,6 +21,34 @@ int main(){
    scanf("%f",&num2);
    elevarAlCuadrado2(&num2);
    printf("su cuadrado es %.2f\n", num2);
+
+    printf("ingrese un valor entero: \n");
+    scanf("%d", &variable);
+    printf("la direccion de memoria de la varia es %p\n", &variable);
+    printf("el contenido de la variable es: %d\n", variable);
+
+    printf("Ingrese un par de valores enteros\nIngrese a:");
+    scanf("%d", &a);
+    printf("Ingrese b:");
+    scanf("%d", &b);
+
+    Invertir(&a,&b);
+    printf("invirtiendo...\n");
+
+    printf("el valor de a es: %d\n", a);
+    printf("el valor de b es: %d\n", b);
+
+
+    printf("Ingrese un par de valores enteros\nIngrese a:");
+    scanf("%d", &a);
+    printf("Ingrese b:");
+    scanf("%d", &b);
+
+    orden(&a,&b);
+    printf("ordenando...\n");
+
+    printf("el valor de a es: %d\n", a);
+    printf("el valor de b es: %d\n", b);
 
    return 0;
 }
@@ -28,6 +61,24 @@ float elevarAlCuadrado(float numero){
 
 void elevarAlCuadrado2(float *numero){
    *numero = pow(*numero,2);
+}
+
+void Invertir(int *a,int *b){
+    int aux;
+    aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void orden(int *a,int *b){
+    int aux;
+    if (*b > *a)
+    {
+        aux = *b;
+        *b = *a;
+        *a = aux;
+    }
+    
 }
 
 
